f7bae71c3543cdbadde04cf9b266aa3d186c9098
linmt <549343315@qq.com>
2018-11-21 23:16:42 +0800

    181121  搭建环境

---
+++ src/main/java/com/lmt/dao/CostDao.java
@@ -0,0 +1,20 @@
+package com.lmt.dao;
+
+import com.lmt.annotation.MybatisDao;
+import com.lmt.entity.Cost;
+import com.lmt.entity.CostSimple;
+
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * Created by 张洲徽 on 2018/11/1.
+ */
+@MybatisDao
+public interface CostDao {
+    void save(Cost cost);
+    List<Cost> findAll();
+    List<Cost> findByPage(HashMap map);
+    int findTotalPage(int pageSize);
+    List<CostSimple> findAllCostSimple();
+}
