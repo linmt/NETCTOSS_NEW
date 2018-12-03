ca74c8675e430e289e5bd2d1dee771e2fe6c1118
linmt <43060283+linmt@users.noreply.github.com>
2018-12-03 15:28:17 +0800

    181203    注册功能完成服务器部分

--- src/test/java/TestUserDao.java
+++ src/test/java/TestUserDao.java
@@ -1,8 +1,9 @@
 import com.lmt.dao.UserDao;
 import com.lmt.entity.User;
+import com.lmt.util.NoteUtil;
 
 import javax.annotation.Resource;
-import java.util.List;
+import java.security.NoSuchAlgorithmException;
 
 /**
  * Created by 张洲徽 on 2018/11/26.
@@ -12,8 +13,18 @@ public class TestUserDao extends BaseJunit4Test {
     private UserDao userdao;
     @org.junit.Test  //标明是测试方法
     //@Ignore  //表示该方法被忽略
-    public void find( ) {
-        User user=userdao.findByName("zhouj");
-        System.out.println(user);
+    public void find( ) throws NoSuchAlgorithmException {
+//        User user=userdao.findByName("zhouj");
+//        System.out.println(user);
+
+        User user = new User();
+        user.setCn_user_name("ddd");//用户名
+        user.setCn_user_desc("demo");//昵称
+        String md5_password = NoteUtil.md5("1234");
+        user.setCn_user_password(md5_password);//设置密码
+        user.setCn_user_token(null);//令牌
+        String userId = NoteUtil.createId();
+        user.setCn_user_id(userId);//用户ID
+        userdao.save(user);
     }
 }
